b9ec1d9c0e08ef5031b9f178a3c5149f76e06b80
PauloVicente89 <pauloviana089@gmail.com>
2022-11-30 13:25:44 -0300

    test commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # The intention of the code is seem the max with a human
 # made to circumvent sites where it is not possible to use the 'ctrl + V' function
 
-
+#
 # How to use?
 # 1 - Copy the text
 # 2 - Paste the text on archive 'text.txt'
